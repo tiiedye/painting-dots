bff9e30181c20066741d5dc02cdaea9db5c58b97
tiiedye <tiiedye@gmail.com>
2021-12-07 14:21:52 -0800

    writing code to draw dots to make randomly generated dots

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 # Painting Dots
 import colorgram
 import turtle
+import random
 
 rgb_colors = []
 colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    rgb_colors.append(color.rgb)
+turtle.colormode(255)
 
-print(rgb_colors)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
+cursor = turtle.Turtle()
+cursor.penup()
+
+x_coord = -250
+y_coord = -225
+
+cursor.goto(x_coord, y_coord)
+
+
+def draw_row():
+    for dot in range(10):
+        cursor.dot(10, random.choice(rgb_colors))
+        cursor.fd(50)
+
+
+for i in range(10):
+    draw_row()
+    y_coord += 50
+    cursor.goto(x_coord, y_coord)
+
+cursor.hideturtle()
+
+# Exit canvas
+screen = turtle.Screen()
+screen.exitonclick()
