2dbb62e7b8c38a2b994f5f56eb24f2a519a44598
tiiedye <tiiedye@gmail.com>
2021-12-07 13:56:09 -0800

    initial files

---
+++ main.py
@@ -0,0 +1,10 @@
+# Painting Dots
+import colorgram
+import turtle
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    rgb_colors.append(color.rgb)
+
+print(rgb_colors)
